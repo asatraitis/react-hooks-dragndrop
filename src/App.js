96290e1fc8eb026ba846f8881126038d2852b1d8
Andy <asatraitis@allegisgroup.com>
2020-02-17 18:46:59 -0500

    Added styles and data structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const data = [
+  {title: 'group 1', items: ['1', '2', '3']},
+  {title: 'group 2', items: ['4', '5']}
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="drag-n-drop">
+        {data.map((grp, grpI) => (
+          <div key={grp.title} className="dnd-group">
+            {grp.items.map((item, itemI) => (
+              <div draggable key={item} className="dnd-item">
+                {item}
+              </div>
+            ))}
+          </div>
+        ))}
+        </div>
+
+          {/* <div className="drag-n-drop">
+            <div className="dnd-group">
+              <div className="group-title">Group 1</div>
+              <div className="dnd-item">
+                <div>
+                  <p>ITEM 1</p>
+                </div>
+              </div>
+              <div className="dnd-item">
+                <div>
+                  <p>ITEM 2</p>
+                </div>
+              </div>
+              <div className="dnd-item">
+                <div>
+                  <p>ITEM 3</p>
+                </div>
+              </div>
+            </div>
+            <div className="dnd-group">
+            <div className="group-title">Group 1</div>
+              <div className="dnd-item">
+                <div>
+                  <p>ITEM 1</p>
+                </div>
+              </div>
+              <div className="dnd-item">
+                <div>
+                  <p>ITEM 2</p>
+                </div>
+              </div>
+            </div>
+          </div> */}
       </header>
     </div>
   );
